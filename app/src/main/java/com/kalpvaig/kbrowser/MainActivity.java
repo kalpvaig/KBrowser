ef8ff2909e67ed93a45e7eefbff90df12842c57a
Shashwat Singh <shashwat@kalpvaig.com>
2016-10-08 12:36:43 +0530

    fixed network issue and enabled javascript

--- app/src/main/java/com/kalpvaig/kbrowser/MainActivity.java
+++ app/src/main/java/com/kalpvaig/kbrowser/MainActivity.java
@@ -1,9 +1,15 @@
 package com.kalpvaig.kbrowser;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.kalpvaig.kbrowser.Setting.Prefs;
 
@@ -19,15 +25,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         getSupportActionBar().setTitle(Prefs.getPreference(this,"appname","KBrowser"));
 
         String url = Prefs.getPreference(this,"url","http://kalpvaig.com");
 
         webView = (WebView) findViewById(R.id.website);
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
 
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+            startActivity(new Intent(MainActivity.this,Settings.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
--- app/src/main/java/com/kalpvaig/kbrowser/Settings.java
+++ app/src/main/java/com/kalpvaig/kbrowser/Settings.java
@@ -1,10 +1,13 @@
 package com.kalpvaig.kbrowser;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.kalpvaig.kbrowser.Setting.Prefs;
 
@@ -18,9 +21,24 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         url = (EditText) findViewById(R.id.url);
         appname = (EditText) findViewById(R.id.appname);
 
+        url.setText(Prefs.getPreference(this,"url","http://kalpvaig.com"));
+        appname.setText(Prefs.getPreference(this,"appname","KBrowser"));
         saveButton = (Button) findViewById(R.id.saveButton);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +47,11 @@ public class Settings extends AppCompatActivity {
 
                 Prefs.saveToPreferences(Settings.this,"url",url.getText().toString());
                 Prefs.saveToPreferences(Settings.this,"appname",appname.getText().toString());
+
+                Toast.makeText(Settings.this, "Successfully Saved", Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(Settings.this,MainActivity.class));
+
             }
         });
     }
